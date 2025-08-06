7a53b5c521d9f5db589a43f0b13db6e4d9db14ff
prappo4 <nava79682@gmail.com>
2025-08-07 03:12:44 +0600

    Update script.js
    added script.js

--- script.js
+++ script.js
@@ -399,7 +399,7 @@ function copyReferralLink() {
 
 // External link functions
 function joinCommunity() {
-    window.open('https://t.me/Skyllorofficialchannel', '_blank');
+    window.open('https://t.me/Skyllorofficial', '_blank');
 }
 
 function contactTeam() {
